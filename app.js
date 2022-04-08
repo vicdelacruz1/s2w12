bd754ef11f00c31bf25ddd41ee1800839fedba7a
De La Cruz <vicdelacruz@FVFY377TJ1WK.tld>
2022-04-08 09:33:06 -0500

    fourth commit

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+let userNum = document.getElementById("getNumber");
+userNum.addEventListener("keydown", function (e) {
+    if (e.keyCode == 13) {
+        run(e);
+    }
+});
+
+
+userNum.focus();
+
+let counter = 0;
+
+
 function run() {
     document.getElementById("paragraph").innerHTML = "Hello World!";
     document.getElementById("paragraph").style.backgroundColor = "green";
@@ -35,6 +48,7 @@ function compareNumbers() {
     let a = userNumber();
     let b = randomNum();
     let z = document.getElementById("compare");
+    let c = document.getElementById("counter");
 
     if ( a != b) {
         z.innerHTML = "Numbers are not the same. Computer got " + b + ", and User got " + a;
@@ -42,11 +56,30 @@ function compareNumbers() {
         z.style.backgroundColor = "black"
         z.style.padding = "20px"
         z.style.textAlign = "center"
+        counter++
+        c.innerHTML = "You have tried " + counter + " times";
+        c.style.color = "white"
+        c.style.backgroundColor = "black"
+        c.style.padding = "20px"
+        c.style.textAlign = "center"
     } else if ( a == b ) {
         z.innerHTML = "Numbers are the same. Computer got " + b + ", and User got " + a;
         z.style.color = "#ADD8E6"
         z.style.backgroundColor = "gray"
         z.style.padding = "20px"
         z.style.textAlign = "center"
+        c.innerHTML = "I took you " + counter + " attempts, You Idiot.";
+        c.style.color = "white"
+        c.style.backgroundColor = "black"
+        c.style.padding = "20px"
+        c.style.textAlign = "center"
+        counter = 0;
     }
+
+    resetInput();
+}
+
+
+function resetInput() {
+    document.getElementById("getNumber").value = "";
 }
